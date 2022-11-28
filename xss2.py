4db8b18f6a8e7f3244fefaece133077a2f3ded66
diksha-chhabra <101804590+diksha-chhabra@users.noreply.github.com>
2022-11-28 16:32:24 +0530

    Add files via upload
    XSS web scraper

---
+++ xss2.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+ 
+ 
+Target = input("Type your target URL...")
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+ 
+urls = []
+for link in soup.find_all('input'):
+    print(link.get('href'))
